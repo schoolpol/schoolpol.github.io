b55895d271cc5da6e953cda37ef4bf827b81ee46
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-13 20:59:42 +0000

    data/*/*.geojson: use latitude and longitude

--- make-europe-geojson.sh
+++ make-europe-geojson.sh
@@ -9,6 +9,8 @@ for country in IS HR SI LV IT FI FR ES IE DE LU NO EL PT SE \
     EE HU CY RO PL DK AT LT BE SK CZ RS AL BG CH UK NL LI MK MT; do
     echo -n "  $country "
     mkdir -p data/$country
-    ogr2ogr -f GeoJSON -where "CNTR_ID = '$country'" data/$country/$country.geojson "$EUROPE_SHAPEFILE"
+    ogr2ogr -f GeoJSON -where "CNTR_ID = '$country'" \
+        -s_srs "$EUROPE_SHAPEFILE.prj" -t_srs EPSG:4326 \
+        data/$country/$country.geojson "$EUROPE_SHAPEFILE.shp"
     echo "✔"
 done
