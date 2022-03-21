0295b08441d41a9e8186137aef081546c2ff1a33
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-21 17:04:30 +0000

    nix: add all project dependencies

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ gdal ];
+  buildInputs = with pkgs; [
+    gdal
+    nodePackages.npm
+    nodejs
+    python3
+    python39Packages.pandas
+    python39Packages.toml
+    python39Packages.pycountry
+  ];
 }
