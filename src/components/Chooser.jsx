36e95c60de6e0657b57ce369844256bb0f199722
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-21 17:04:45 +0000

    components(Chooser): add gender dropdown

--- src/components/Chooser.jsx
+++ src/components/Chooser.jsx
@@ -11,6 +11,11 @@ class Chooser extends React.Component {
         <select name="variables" id="variables">
           <option value="ed_isced_6to8">ISCED 6 - 8</option>
         </select>
+        <select name="gender" id="gender">
+          <option value="">All genders</option>
+          <option value="_f">Female</option>
+          <option value="_m">Male</option>
+        </select>
         <input
           type="range"
           min="1"
