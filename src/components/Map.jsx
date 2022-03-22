c68de826203a2a8361567c59a8206f521c80a174
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-22 18:31:10 +0000

    components(Map): move Chooser here

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -144,6 +144,30 @@ class Map extends React.Component {
         </span>
       );
     return (
+      <div id="content">
+      <div id="chooser">
+        <p>Select a country to get started ⟶</p>
+        <select name="country" id="country">
+          <option value="UK">United Kingdom</option>
+        </select>
+        <select name="variables" id="variables">
+          <option value="ed_isced_6to8">ISCED 6 - 8</option>
+        </select>
+        <select name="gender" id="gender">
+          <option value="">All genders</option>
+          <option value="_f">Female</option>
+          <option value="_m">Male</option>
+        </select>
+        <input
+          type="range"
+          min="1"
+          max="100"
+          defaultValue="50"
+          className="slider"
+          id="year"
+        />
+      </div>
+
       <div id="map">
         <div id="lauInfo">{hoverMessage}</div>
         <Legend />
@@ -160,6 +184,7 @@ class Map extends React.Component {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </MapContainer>
+      </div>
       </div>
     );
   }
