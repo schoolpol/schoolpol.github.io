f3450e5eb20d2c9e71c210fe26a20cff56512e0a
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-22 23:06:06 +0000

    Move palette functions into module

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,57 +3,14 @@
 
 import React from "react";
 import { GeoJSON, MapContainer, TileLayer } from "react-leaflet";
-import colorbrewer from "colorbrewer";
 
-// replace with fetch() calls according to current state
+import { getColor, legendValues } from "../palette";
 import countries from "../countries.json";
 import index from "../dataindex.json";
+
 const position = [54.505, -0.09];
 
-const defaultColor = "#eeeeee"; // gray
-
 const url = "https://data.trenozoic.net/schoolpol";
-
-const scales = {
-  ed_isced_6to8: {
-    palette: colorbrewer.OrRd,
-    thresholds: [0, 0.5, 1, 2, 5, 10, 15, 20, 30, 100],
-  },
-  ed_isced_3to5: {
-    palette: colorbrewer.BuPu,
-    thresholds: [0, 5, 10, 15, 20, 30, 40, 50, 60, 100],
-  },
-  ed_isced_0to2: {
-    palette: colorbrewer.YlGn,
-    thresholds: [0, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-  },
-};
-
-function getColor(variable, value) {
-  if (variable === undefined) return defaultColor;
-  let scale = scales[variable];
-  let ncolors = scale.thresholds.length - 1;
-  let palette = scale.palette[ncolors];
-  for (let i = 0; i < scale.thresholds.length; i++) {
-    if (value < scale.thresholds[i]) {
-      return palette[i - 1];
-    }
-  }
-  return defaultColor;
-}
-
-function legendValues(variable) {
-  let values = [];
-  let scale = scales[variable];
-  let palette = scale.palette[scale.thresholds.length - 1];
-  for (let i = 0; i < scale.thresholds.length - 1; i++) {
-    values.push({
-      color: palette[i],
-      text: `${scale.thresholds[i]} - ${scale.thresholds[i + 1]}`,
-    });
-  }
-  return values;
-}
 
 class Map extends React.Component {
   constructor(props) {
