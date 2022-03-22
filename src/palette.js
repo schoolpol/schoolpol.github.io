223bee4dd7d8ea5c86a3ade744f9cf93a73192e7
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-22 23:06:06 +0000

    Move palette functions into module

---
+++ src/palette.js
@@ -0,0 +1,43 @@
+import colorbrewer from "colorbrewer";
+const defaultColor = "#eeeeee"; // gray
+
+const scales = {
+  ed_isced_6to8: {
+    palette: colorbrewer.OrRd,
+    thresholds: [0, 0.5, 1, 2, 5, 10, 15, 20, 30, 100],
+  },
+  ed_isced_3to5: {
+    palette: colorbrewer.BuPu,
+    thresholds: [0, 5, 10, 15, 20, 30, 40, 50, 60, 100],
+  },
+  ed_isced_0to2: {
+    palette: colorbrewer.YlGn,
+    thresholds: [0, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+  },
+};
+
+export function getColor(variable, value) {
+  if (variable === undefined) return defaultColor;
+  let scale = scales[variable];
+  let ncolors = scale.thresholds.length - 1;
+  let palette = scale.palette[ncolors];
+  for (let i = 0; i < scale.thresholds.length; i++) {
+    if (value < scale.thresholds[i]) {
+      return palette[i - 1];
+    }
+  }
+  return defaultColor;
+}
+
+export function legendValues(variable) {
+  let values = [];
+  let scale = scales[variable];
+  let palette = scale.palette[scale.thresholds.length - 1];
+  for (let i = 0; i < scale.thresholds.length - 1; i++) {
+    values.push({
+      color: palette[i],
+      text: `${scale.thresholds[i]} - ${scale.thresholds[i + 1]}`,
+    });
+  }
+  return values;
+}
