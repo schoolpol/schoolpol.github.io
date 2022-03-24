12cb731daacebce881302a59876b9dce3b92419a
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-24 12:55:03 +0000

    geojson: generate GR, drop FR (use mapshaper ver)

--- make-europe-geojson.sh
+++ make-europe-geojson.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 echo Generating geojson for Europe
 EUROPE_SHAPEFILE=$(tomlq -r .shapefiles.europe config.toml)
 
-for country in IS HR SI LV IT FI FR ES IE DE LU NO EL PT SE \
+for country in IS HR SI LV IT FI GR ES IE DE LU NO EL PT SE \
     EE HU CY RO PL DK AT LT BE SK CZ RS AL BG CH UK NL LI MK MT; do
     echo -n "  $country "
     mkdir -p data/$country
