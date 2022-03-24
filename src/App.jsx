7c67b5b51aa5c7d41505fea35ab3635ecba53361
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-24 08:43:10 +0000

    Add about modal and improve style

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Map from "./components/Map";
+import About from "./components/About";
 
 export default function App() {
   return (
     <div>
       <div id="blurb">
-        <h1>Schoolpol: The Transformation of Post War Education</h1>
-        <p>
-          This project shows the educational attainments across 22 countries for
-          10 variables.&nbsp;
-          <a href="https://schoolpol.web.ox.ac.uk/home">Read more...</a>
-        </p>
-      </div>
-      <Map />
+        <h1><About /> Schoolpol: The Transformation of Post War Education</h1>
+     </div>
+     <Map />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Popup from "reactjs-popup";
+
+const About = () => (
+  <Popup trigger={<button className="button">About</button>} modal>
+    <div id="aboutmodal">
+    <p>
+    <strong>Schoolpol: The Transformation of Post-War Education</strong>
+    &nbsp;is a project based at the
+    &nbsp;<a href="https://www.politics.ox.ac.uk">Department of Political Science</a>&nbsp;
+    at Oxford, which looks at how policies have affected education after the second
+      world war. We look at <strong>21</strong> countries across a range of factors.
+    </p>
+    <p>This map visualises the&nbsp;
+      <a href="http://uis.unesco.org/en/topic/international-standard-classification-education-isced">
+        International Standard Classification of Education (ISCED)
+      </a> levels for these countries. We look at three levels: ISCED 0 - 2, 3 - 5, 6 - 8
+      which you can select in the dropdown. The data can also be visualised by gender
+      and across time.
+    </p>
+      <span style={{color: "#999999", "font-size": "small"}}>Tap [esc] or click outside to close</span>
+    </div>
+  </Popup>
+);
+
+export default About;
