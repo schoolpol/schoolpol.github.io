621362a5a6ae026fa039113d7f27115c8cb9f31e
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-27 20:59:03 +0100

    map: use stadia dark theme for better contrast

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -204,8 +204,8 @@ class Map extends React.Component {
               />
             )}
             <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
             />
           </MapContainer>
         </div>
