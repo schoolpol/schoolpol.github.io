10dba2f981e99c620868b075a78461e37ee54628
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-03-27 21:49:45 +0100

    Add extended blurb, with mention of About modal

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ export default function App() {
     <div>
       <div id="blurb">
         <h1><About /> Schoolpol: The Transformation of Post War Education</h1>
+        <p>Project looking at how policies have affected education after World War II; this map shows
+          how <abbr title="International Standard Classification of Education">ISCED</abbr> levels within countries, across time.
+          Tap About for more.
+        </p>
      </div>
      <Map />
     </div>
